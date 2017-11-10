018542ecc3c0a96a5b2c9cfbdb1a12ae1975bebd
Hack Reactor Student <student@hackreactor.com>
2017-11-10 12:41:08 -0800

    Render sample data

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const ListItem = (props) => (
   <div>
-    { props.item.description }
+    ebay item id: 
+      <a href={props.auction.viewItemURL}>
+        { props.auction.description }
+      </a>  
+    <br/>
+    auction title: { props.auction.title }
+    <br/>
+    <img src={ props.auction.galleryURL }/>
+    current price: { props.auction.sellingStatus[0].currentPrice[0]["__value__"]}
   </div>
 )
 
