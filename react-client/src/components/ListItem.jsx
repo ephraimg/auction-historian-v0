0bf277df4a5505bcf717056ca1cd06607f04ac94
Hack Reactor Student <student@hackreactor.com>
2017-11-10 15:24:37 -0800

    Add basic styling

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const ListItem = (props) => (
-  <div>
-    ebay item id: 
-      <a href={props.auction.viewItemURL}>
-        { props.auction.description }
-      </a>  
+  <div className="auction-item cf">
+    <img className="auction-img" src={props.auction.galleryURL}/>
+    <button className="save-button" onClick={props.handleSaveClick}>Save</button>
     <br/>
-    auction title: { props.auction.title }
+    ebay item id:
+    <a href={props.auction.viewItemURL}>
+      {props.auction.itemId}
+    </a>  
     <br/>
-    <img src={ props.auction.galleryURL }/>
-    current price: { props.auction.sellingStatus[0].currentPrice[0]["__value__"]}
+    auction title: {props.auction.title}
+    <br/>
+    current price: {'$' + Number(props.auction.sellingStatus[0].currentPrice[0]["__value__"]).toFixed(2)}
   </div>
 )
 
