6a0b4c3e4ecdc9317344865abc5bc0e794423070
Hack Reactor Student <student@hackreactor.com>
2017-11-10 16:32:00 -0800

    Complete database schema, save function

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import $ from 'jquery';
 
-const ListItem = (props) => (
-  <div className="auction-item cf">
-    <img className="auction-img" src={props.auction.galleryURL}/>
-    <button className="save-button" onClick={props.handleSaveClick}>Save</button>
-    <br/>
-    ebay item id:
-    <a href={props.auction.viewItemURL}>
-      {props.auction.itemId}
-    </a>  
-    <br/>
-    auction title: {props.auction.title}
-    <br/>
-    current price: {'$' + Number(props.auction.sellingStatus[0].currentPrice[0]["__value__"]).toFixed(2)}
-  </div>
-)
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  
+
+  
+  render() {
+    return (<div className="auction-item cf">
+      <img className="auction-img" src={props.auction.galleryURL}/>
+      <button className="save-button" 
+              onClick={() => props.handleSaveClick(props.auction)} >
+                Save
+      </button>
+      <br/>
+      ebay item id:
+      <a href={props.auction.viewItemURL}>
+        {props.auction.itemId}
+      </a>  
+      <br/>
+      auction title: {props.auction.title}
+      <br/>
+      current price: {'$' + Number(props.auction.sellingStatus[0].currentPrice[0]["__value__"]).toFixed(2)}
+    </div>)   
+  }
+  
+}
 
 export default ListItem;
