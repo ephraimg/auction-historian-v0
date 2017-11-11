d4b834367eea97c3f2dca519082346b3e5b1695e
Hack Reactor Student <student@hackreactor.com>
2017-11-10 19:22:44 -0800

    Implement save button / saved status

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import $ from 'jquery';
 import ListItem from './ListItem.jsx';
 
-const List = (props) => (
-  <div>
-    <h4> List Component </h4>
-    There are { props.auctions.length } auctions.
-    { props.auctions.map(auction => 
-      <ListItem auction={auction} handleSaveClick={props.handleSaveClick} />
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedAuctions: {}
+    };
+    this.addToSaved = this.addToSaved.bind(this);
+  }
+  
+  componentDidUpdate() {
+    console.log('Update: ', JSON.stringify(this.state.savedAuctions))    
+  }
+  
+  addToSaved(itemId) {
+    console.log('Saved auctions: ', JSON.stringify(this.state.savedAuctions));
+    var newItem = {};
+    newItem[itemId] = true;        
+    var newSaved = $.extend({}, this.state.savedAuctions, newItem);
+    this.setState({
+      savedAuctions: newSaved
+    }); 
+  }
+
+  render() {
+    return (<div>
+      <h4> List Component </h4>
+      There are { this.props.auctions.length } auctions.
+      { this.props.auctions.map(auction => 
+        <ListItem 
+          auction={auction}
+          savedList={this.state.savedAuctions} 
+          handleSaveClick={this.props.handleSaveClick}
+          addToSaved={this.addToSaved} 
+        />
       )}
-  </div>
-)
+    </div>)
+  }
+   
+}
 
 export default List;
